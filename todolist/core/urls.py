c46a4c7e5357b3b5e4b79de0ab54d6e70809e7c8
saybe <Sayberol>
2022-07-09 14:46:07 +0500

    hw 35

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 urlpatterns = [
     # path('login', views.LoginView.as_view(), name='login'),
     path('login', views.LoginView.as_view(), name='login'),
-    # path('logout', views.LogoutView.as_view(), name='logout'),
+    path('profile', views.ProfileView.as_view(), name='profile'),
+    path('signup', views.SignupView.as_view(), name='signup'),
 ]
